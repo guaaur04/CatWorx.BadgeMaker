b2f93fa6b88437d63768fe08bb13ce269b5c6607
V Guardado <auricia.guardado@gmail.com>
2021-03-26 19:12:33 -0700

    complete print

--- Program.cs
+++ Program.cs
@@ -32,11 +32,11 @@ namespace CatWorx.BadgeMaker
             return employees;
         }
 
-        static void PrintEmployees (List<string> employees)
+        static void PrintEmployees (List<Employee> employees)
         {
             for(int i=0; i < employees.Count; i++)
             {
-                string template = "{0,-10\t{1,-20}\t{2}";
+                string template = "{0,-10}\t{1,-20}\t{2}";
                 Console.WriteLine(String.Format(template, employees[i].GetId(), employees[i].GetName(), employees[i].GetPhotoUrl()));
                    
             }
@@ -44,7 +44,7 @@ namespace CatWorx.BadgeMaker
 
         static void Main(string[] args)
         {
-            List<string> employees = GetEmployees();
+            List<Employee> employees = GetEmployees();
             PrintEmployees(employees);
         }
 
